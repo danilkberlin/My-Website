56f9f458b1bb0762f40b8cfdc8c233e1c2afbea8
danilkberlin <danilkravets19@gmail.com>
2024-04-14 19:46:21 +0200

    Modal window

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,26 @@
 import { useState } from 'react';
-import {Container, Nav, Navbar, Offcanvas, Form, Button, Modal} from 'react-bootstrap';
+import {Container, Nav, Navbar, Offcanvas, Form, Button, Modal, Col, Row, InputGroup} from 'react-bootstrap';
 
 function NavBar() {
+
+    //  Modal window 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    //  Forms validation
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false){
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
 
   return (
     <>
@@ -46,9 +61,57 @@ function NavBar() {
         {/* Modals window */}
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Let's make something awesome together!</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+            <Modal.Body>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} md="6" controlId="validationCustom01">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="First name"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="6" controlId="validationCustom02">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Last name"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="12" controlId="validationCustomUsername">
+                    <Form.Label>Email</Form.Label>
+                    <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                        <Form.Control
+                        type="email"
+                        placeholder="Email"
+                        aria-describedby="inputGroupPrepend"
+                        required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                        Please choose a eimal.
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col} md="12" controlId="validationCustom03">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control className='my-2 contact-style' as="textarea" placeholder='A Few Words*' rows={3} />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a Message.
+                    </Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+                <Button type="submit">Submit form</Button>
+            </Form>
+            </Modal.Body>
             <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
                 Close
