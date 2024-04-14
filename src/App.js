720987233d075facb2fadc63f410e5397a20df6c
danilkberlin <danilkravets19@gmail.com>
2024-04-14 20:03:12 +0200

    undate home page but this page have't style

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import NavBar from "./components/NavBar";
+import Top from './Pages/Top';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Top />
     </>
   );
 }
---
+++ src/Pages/Top.js
@@ -0,0 +1,47 @@
+import {Container, Row, Col, Form, Button, Nav, Image} from 'react-bootstrap';
+import BackgroundImage from '../assets/img/background-top1.png';
+
+const Top = () => {
+    return(
+       <>
+            <section id='home' className='home'>
+                <Container>
+                    <Image src={BackgroundImage} className='z-n1 position-absolute ' fluid/>
+                    <Row className='justify-content-center'>
+                        <Col md={6}>
+                            <Row>
+                                <Col className="my-3">
+                                    <button className='px-4 py-1 my-3'><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-sign-railroad" viewBox="0 0 16 16">
+                                    <path d="M11.303 6.584h1.064c.592 0 .936.334.936.844a.79.79 0 0 1-.485.748l.536 1.074h-.59l-.467-.994h-.473v.994h-.521zm.521.414v.861h.46c.292 0 .474-.14.474-.421 0-.286-.188-.44-.467-.44zm-8.771-.414h1.064c.592 0 .936.334.936.844 0 .39-.242.654-.485.748l.536 1.074h-.59l-.467-.994h-.473v.994h-.521zm.521.414v.861h.46c.292 0 .474-.14.474-.421 0-.286-.188-.44-.467-.44z"/>
+                                    <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L4.923 3.861 8 6.939l3.078-3.077L8.35 1.134Zm3.788 3.788L9.061 8l3.077 3.078 2.728-2.728a.495.495 0 0 0 0-.7zm-1.06 7.215L8 9.061l-3.077 3.077 2.727 2.728a.495.495 0 0 0 .7 0zm-7.216-1.06L6.939 8 3.862 4.923 1.134 7.65a.495.495 0 0 0 0 .7z"/>
+                                    </svg> Let's meet!</button>
+                                </Col>
+                                <Row>
+                                    <Col>
+                                        <h1 className=''>I'm Danylo Kravets</h1>
+                                        <h1 className=''>UI/UX Designer and frontend developer.</h1>
+                                    </Col>
+                                </Row>
+                            </Row>
+                            <Row> 
+                                <Col className="my-3"> 
+                                    <Form className='d-flex'>
+                                        <button className='px-4 py-1 mx-2'><span class='text-lets-talk'>My Works<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ui-radios-grid" viewBox="0 0 16 16">
+                                        <path d="M3.5 15a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5m9-9a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5m0 9a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5M16 3.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-9 9a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m5.5 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m-9-11a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m0 2a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                                        </svg></span></button>
+                                        <button className='px-4 py-1 mx-2'>Download CV <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                                        </svg></button>
+                                    </Form>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+       </> 
+    )
+}
+
+export default Top
