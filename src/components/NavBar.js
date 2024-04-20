6c074ad266c1908598ae92a93777f6bdcc2dad5a
danilkberlin <danilkravets19@gmail.com>
2024-04-20 19:21:22 +0200

    update root

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,7 +55,7 @@ function NavBar() {
                             </label>
                         </Form>
                         <Form className="d-flex align-items-center">
-                            <Button className='w-100  rounded-4' variant="px-4 py-1 mx-2" onClick={handleShow}><span className='text-lets-talk'>Let's Talk  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                            <Button className='form-nav-button w-100  rounded-4' variant="px-4 py-1 mx-2" onClick={handleShow}><span className='text-lets-talk'>Let's Talk  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
                             <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
                             <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
                             </svg></span>
