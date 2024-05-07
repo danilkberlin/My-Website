4157bf2638355d1f4efb5a44cfacdea3be089fd1
danilkberlin <danilkravets19@gmail.com>
2024-05-08 00:03:24 +0200

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,7 +26,7 @@ function NavBar() {
     <>
         <Navbar expand='md' className="navbar-fixed mb-3">
             <Container fluid>
-                <Navbar.Brand href="#" className='brand'>LOGO</Navbar.Brand>
+                {/* <Navbar.Brand href="#" className='brand'>LOGO</Navbar.Brand> */}
                 <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
                 <Navbar.Offcanvas
                     id='md'
@@ -120,7 +120,7 @@ function NavBar() {
                     </Form.Control.Feedback>
                     </Form.Group>
                 </Row>
-                <Button type="submit" className='anim-button black-text rounded-4'>Send Messeage <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                <Button type="submit" className='anim-button  rounded-4'>Send Messeage <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
                     </svg>
                 </Button>
@@ -141,3 +141,5 @@ function NavBar() {
 }
 
 export default NavBar;
+
+
