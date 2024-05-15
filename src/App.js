b1d130158b2292c22932c53f9f167d1db076ead4
danilkberlin <danilkravets19@gmail.com>
2024-05-16 00:01:02 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import NavBar from "./components/NavBar";
-import Top from './Pages/Top';
-import Portfolio from './Pages/Portfolio';
-import About from './Pages/About';
-import Resume from './Pages/Resume';
-import Comment from './Pages/Comment';
-import Contact from './Pages/Contact';
-import Footer from './Pages/Footer';
-import ScrollSpy from 'react-ui-scrollspy';
+import Top from "./Pages/Top";
+import Portfolio from "./Pages/Portfolio";
+import About from "./Pages/About";
+import Resume from "./Pages/Resume";
+import Comment from "./Pages/Comment";
+import Contact from "./Pages/Contact";
+import Footer from "./Pages/Footer";
+import ScrollSpy from "react-ui-scrollspy";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
       <ScrollSpy>
         <Top />
         {/* ===== Portfolio wird noch gearbeiten =========*/}
-                            <Portfolio /> 
+        <Portfolio />
         {/* ============================================= */}
         <About />
         <Resume />
